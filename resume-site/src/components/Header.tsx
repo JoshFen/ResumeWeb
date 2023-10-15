1a455637b0ae33ba289d15310bb1b9125c1b98f1
JoshFen <JFentress15@gmail.com>
2023-10-15 11:20:10 -0400

    First Push

--- resume-site/src/components/Header.tsx
+++ resume-site/src/components/Header.tsx
@@ -1,7 +1,6 @@
 function Header() {
 
-    return <h1>Joshua Fentress</h1>;
-
+    return (<h1>Joshua Fentress</h1>)
 }
 
 export default Header
---
+++ resume-site/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return <nav className="nav">
+        <ul>
+            <li> <a href="#about-me">About Me</a> </li>
+            <li> <a href="#projects">Projects</a> </li>
+            <li> <p>RESUME-HERE</p> </li>
+            <li> <a href="#contact">Contact</a> </li>
+        </ul>
+
+    </nav>
+}
