e9c5dd6ac976d94c4d013eb7b0e132371ab2a1ca
JoshFen <JFentress15@gmail.com>
2024-02-10 15:45:20 -0500

    setup gh-pages

--- resume-site/vite.config.ts
+++ resume-site/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Resume-Site/' // Added to try and get github pages working, can remove if not.
 })
