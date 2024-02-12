dc841a430c048e10b7965e4285869f0a3c0f00ee
JoshFen <JFentress15@gmail.com>
2024-02-12 15:35:58 -0500

    photos testing

--- resume-site/vite.config.ts
+++ resume-site/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Resume-Site/', // Added to try and get github pages working, can remove if not.
+  base: '/me/', // Added to try and get github pages working, can remove if not.
   build: {
     outDir: 'build', // specify the output directory
   } // Added to try and get github pages working, can remove if not.
